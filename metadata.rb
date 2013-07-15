3c91c6cc02a93840204b5d9493c8604cc754262c
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-07-15 20:05:38 +0200

    First specs

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'orientdb'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Federico Gimenez Nieto'
+maintainer_email 'federico.gimenez@gmail.com'
+license          'Apache-2.0'
 description      'Installs/Configures orientdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'java'
