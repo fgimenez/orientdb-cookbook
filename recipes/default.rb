59a09e8f6382890f3fe5c68482e9d3ac876755bf
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-07-16 18:41:33 +0200

    default recipe complete

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'apt'
+include_recipe 'java'
+include_recipe 'orientdb::user'
+include_recipe 'orientdb::structure'
+include_recipe 'orientdb::resources'
+include_recipe 'orientdb::scripts'
+include_recipe 'orientdb::configuration'
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,1 +1,35 @@
 require_relative 'spec_helper'
+
+describe 'orientdb::default' do
+  let(:runner) { ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '12.04').
+    converge('orientdb::default')}
+
+  it 'includes the apt recipe' do
+    expect(runner).to include_recipe('apt')
+  end
+
+  it 'includes the java recipe' do
+    expect(runner).to include_recipe('java')
+  end
+
+  it 'includes the orientdb::user recipe' do
+    expect(runner).to include_recipe('orientdb::user')
+  end
+
+  it 'includes the orientdb::structure recipe' do
+    expect(runner).to include_recipe('orientdb::structure')
+  end
+
+  it 'includes the orientdb::resources recipe' do
+    expect(runner).to include_recipe('orientdb::resources')
+  end
+
+  it 'includes the orientdb::scripts recipe' do
+    expect(runner).to include_recipe('orientdb::scripts')
+  end
+
+  it 'includes the orientdb::configuration recipe' do
+    expect(runner).to include_recipe('orientdb::configuration')
+  end
+
+end
